a5ec14b61f03cf45bb4adeb38d921b0f95a73ca7
kat <k.lukens@gmail.com>
2025-08-01 18:16:33 -0600

    debug now called with query paramater

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,4 @@
-export const DEBUG = true;
+const urlParams = new URLSearchParams(window.location.search);
+const debugParam = urlParams.get('debug');
+
+export const DEBUG = ['true', '1', 'on', 'yes'].includes(debugParam?.toLowerCase()) || false;
